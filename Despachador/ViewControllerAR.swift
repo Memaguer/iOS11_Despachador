f82ae150238225f19e6bfe7293aea2416736019e
Guillermo Barrientos <barrientos.memo@gmail.com>
2017-11-25 13:16:01 -0600

    Implementación de ARKit funcionando correctamente

--- Despachador/ViewControllerAR.swift
+++ Despachador/ViewControllerAR.swift
@@ -19,17 +19,22 @@ class ViewControllerAR: UIViewController, ARSKViewDelegate, CLLocationManagerDel
     
     let locationManager = CLLocationManager()
     var userLocation = CLLocation()
+    
     var sitesJSON : JSON!
-    var sites = [UUID: String]()
-    var userHeading  = 0.0
+    
+    var userHeading = 0.0
     var headingStep = 0
+    
+    var sites = [UUID : String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
+        
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -42,8 +47,6 @@ class ViewControllerAR: UIViewController, ARSKViewDelegate, CLLocationManagerDel
         if let scene = SKScene(fileNamed: "Scene") {
             sceneView.presentScene(scene)
         }
-        
-        self.navigationController?.navigationBar.tintColor = UIColor.white;
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -51,7 +54,7 @@ class ViewControllerAR: UIViewController, ARSKViewDelegate, CLLocationManagerDel
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -71,6 +74,7 @@ class ViewControllerAR: UIViewController, ARSKViewDelegate, CLLocationManagerDel
     // MARK: - ARSKViewDelegate
     
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+        
         let labelNode = SKLabelNode(text: sites[anchor.identifier])
         labelNode.horizontalAlignmentMode = .center
         labelNode.verticalAlignmentMode = .center
@@ -105,6 +109,9 @@ class ViewControllerAR: UIViewController, ARSKViewDelegate, CLLocationManagerDel
         
     }
     
+    
+    //MARK: CLLocationManager
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error.localizedDescription)
     }
@@ -115,88 +122,142 @@ class ViewControllerAR: UIViewController, ARSKViewDelegate, CLLocationManagerDel
         }
     }
     
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else {
             return
         }
+        
         userLocation = location
         
         DispatchQueue.global().async {
-            self.updateSite()
+            self.updateSites()
         }
+        
     }
     
+    
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
-        DispatchQueue.global().async {
+        
+        DispatchQueue.main.async {
             self.headingStep += 1
             
-            if self.headingStep < 2 { return }
+            if self.headingStep < 3 { return }
             
-            self.userHeading = newHeading.magneticHeading
+            self.userHeading = newHeading.trueHeading
             self.locationManager.stopUpdatingHeading()
             self.createSites()
         }
+        
     }
     
-    func updateSite() {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(userLocation.coordinate.latitude)%7C\(userLocation.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-        guard let url = URL(string: urlString) else { return }
+    
+    func updateSites(){
+        //let urlString = "https://es.wikipedia.org/w/api.php?ggscoord=\(userLocation.coordinate.latitude)%7C\(userLocation.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        let urlString = "https://kuhni-fb8ab.firebaseio.com/buses.json"
+        guard let url = URL(string: urlString) else {return}
         
         if let data = try? Data(contentsOf: url){
             sitesJSON = JSON(data)
+            
+            print(sitesJSON)
+            
             locationManager.startUpdatingHeading()
         }
+        
     }
     
-    func createSites() {
-        // Get items for JSON
-        for page in sitesJSON["query"]["pages"].dictionaryValue.values {
-            // Get longitude and latitude for sites
-            let lat = page["coordinates"][0]["lat"].doubleValue
-            let lon = page["coordinates"][0]["lon"].doubleValue
+    
+    func createSites(){
+        
+        //Hacer un bucle de todos los lugares que ocupa el JSON de la Wikipedia
+        for index in 0...12 {
+            //Ubicar latitud y longitud de esos lugares -> CLLocation
+            let lat = sitesJSON["result"][index]["latitude"].doubleValue
+            let lon = sitesJSON["result"][index]["longitude"].doubleValue
             let location = CLLocation(latitude: lat, longitude: lon)
-            // Get distances and direction between user and sites
+            
+            //Calcualr la distancia y la dirección (azimut) desde el usuario hasta ese lugar
             let distance = Float(userLocation.distance(from: location))
             let azimut = direction(from: userLocation, to: location)
-            // Get angle between user and site
+            
+            //Sacar ángulo entre azimut y la dirección del usuario
             let angle = azimut - userHeading
-            let angleRad = degreesToRadians(angle)
-            // Create matrix for horizontal rotation (anchor)
+            let angleRad = deg2Rad(angle)
+            
+            //Crear las matrices de rotación para posicionar horizontalmente el ancla
+            print("placa: \(sitesJSON["result"][index]["licensePlate"].string!)")
+            print("angulo: \(angle)")
+            print("distancia: \(distance)")
             let horizontalRotation = simd_float4x4(SCNMatrix4MakeRotation(Float(angleRad), 1, 0, 0))
-            // Create matrix for vertical rotation (distance)
-            let verticalRotation = simd_float4x4(SCNMatrix4MakeRotation(-0.3 + Float(distance/500), 0, 1, 0))
-            // Multiply matrices and ARKit
+            //Crear la matriz para la rotación vertical basada en la distancia
+            //let verticalRotation = simd_float4x4(SCNMatrix4MakeRotation(Float(distance/1000), 0, 1, 0))
+            
+            /*var verticalRot: Float
+            if distance > 10000 {
+                verticalRot = 0.1
+            }
+            else if distance > 5000 {
+                verticalRot = 0
+            }
+            else{
+                verticalRot = -0.1
+            }*/
+            
+            print("clamp: \(clamp(value:distance / 2000, lower: -0.2, upper: 0.3)))")
+            let verticalRotation = simd_float4x4(SCNMatrix4MakeRotation(clamp(value:distance / 100000, lower: -0.2, upper: 0.3), 0, 1, 0))
+            
+            //Multiplicar las matrices de rotación anteriores y multiplicarlas por la cámara de ARKit
             let rotation = simd_mul(horizontalRotation, verticalRotation)
-            // Create a new matrix
+            
+            //Crear una matriz identidad y moverla una cierta cantidad dependiendo de donde posicionar el objeto en profundidad.
             guard let sceneView = self.view as? ARSKView else{ return }
+            
             guard let currentFrame = sceneView.session.currentFrame else { return }
+            
             let rotation2 = simd_mul(currentFrame.camera.transform, rotation)
-            // Put anchor on scene and give it an id
+            
+            //Posicionaremos el ancla y le daremos un identificador para localizarlo en escena
             var translation = matrix_identity_float4x4
-            translation.columns.3.z = -(distance / 1000)
+            translation.columns.3.z = -clamp(value:distance / 1500, lower: 0.2, upper: 4.0)
+            
             let transform = simd_mul(rotation2, translation)
+            
             let anchor = ARAnchor(transform: transform)
             sceneView.session.add(anchor: anchor)
-            sites[anchor.identifier] = "\(page["title"].string!) - \(Int(distance)) metros"
+            sites[anchor.identifier] = "\(sitesJSON["result"][index]["licensePlate"].string!) - \(Int(distance)) metros"
+            
         }
     }
     
-    func degreesToRadians(_ degrees: Double) -> Double {
+    
+    //MARK: Mathematical library
+    
+    func deg2Rad(_ degrees: Double) -> Double {
         return degrees * Double.pi / 180.0
     }
     
-    func radiansToDegrees(_ radians: Double) -> Double {
+    func rad2deg(_ radians: Double) -> Double {
         return radians * 180.0 / Double.pi
     }
     
-    func direction(from p1: CLLocation, to p2: CLLocation) -> Double {
+    
+    ///Returns the input value clamped to the lower and upper limits.
+    func clamp<T: Comparable>(value: T, lower: T, upper: T) -> T {
+        return min(max(value, lower), upper)
+    }
+    
+    //atag2 ( sen(dif longitudes) * cos(long2),
+    //        cos(lat1) * sen(lat2) - sen(lat1) * cos(lat2) * cos (dif long)
+    
+    func direction(from p1:CLLocation, to p2: CLLocation) -> Double {
         let dif_long = p2.coordinate.longitude - p1.coordinate.longitude
         
         let y = sin(dif_long) * cos(p2.coordinate.longitude)
-        let x = cos(p1.coordinate.latitude) * sin(p2.coordinate.latitude) - sin(p1.coordinate.latitude) * cos(p2.coordinate.latitude) * cos (dif_long)
+        let x = cos(p1.coordinate.latitude) * sin(p2.coordinate.latitude) - sin(p1.coordinate.latitude) * cos(p2.coordinate.latitude) * cos(dif_long)
         
-        let atan_rad = atan2(x, y)
+        let atan_rad = atan2(y, x)
         
-        return radiansToDegrees(atan_rad)
+        return rad2deg(atan_rad)
     }
 }
